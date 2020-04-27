6dc29eea19c12fd7d84ec919403000cabeb84a2c
Charles Loder <charles.w.loder@gmail.com>
2020-04-27 08:57:07 -0400

    updated v1.2.0 and options

--- assets/js/transliterate.js
+++ assets/js/transliterate.js
@@ -1,12 +1,22 @@
 const inputBtn = document.querySelector('#input_button');
 const input = document.querySelector('#input');
 const output = document.querySelector('#output');
+const simpleBox = document.querySelector('#isSimple')
 const heb = require('hebrew-transliteration');
 const transliterate = heb.transliterate;
 
+simpleBox.addEventListener('change', (e) => {
+    if (e.target.checked) {
+        output.placeholder = 'ivrit';
+    } else {
+        output.placeholder = 'ʿibrît';
+    }
+})
+
 inputBtn.addEventListener('click', () => {
-  let qametsQatanVal = document.querySelector('#qametsQatan').checked;
-  let hebText = input.value;
-  let transText = transliterate(hebText, {isSequenced: true, qametsQatan: qametsQatanVal}); 
-  output.value = transText;
+    let qametsQatanVal = document.querySelector('#qametsQatan').checked;
+    let isSimpleVal = simpleBox.checked
+    let hebText = input.value;
+    let transText = transliterate(hebText, { isSequenced: true, qametsQatan: qametsQatanVal, isSimple: isSimpleVal });
+    output.value = transText;
 })
