fae5b61b0b45ae803775e9bfd4bf98cd49181b87
Charles Loder <charles.w.loder@gmail.com>
2020-05-14 08:04:49 -0400

    updated about page

--- app.js
+++ app.js
@@ -5,21 +5,22 @@ const pageId = require('spike-page-id')
 const env = process.env.SPIKE_ENV
 
 module.exports = {
-  devtool: 'source-map',
-  ignore: ['**/layout.html', '**/_*', '**/.*', 'readme.md', 'yarn.lock', 'package-lock.json'],
-  entry: {
-    'js/transliterate': ['./assets/js/transliterate.js'],
-    'js/remove': ['./assets/js/remove.js'],
-    'js/sequence': ['./assets/js/sequence.js'],
-    'js/index': ['./assets/js/index.js']
-  },
-  reshape: htmlStandards({
-    locals: (ctx) => { return { pageId: pageId(ctx), foo: 'bar' } },
-    minify: env === 'production'
-  }),
-  postcss: cssStandards({
-    minify: env === 'production',
-    warnForDuplicates: env !== 'production'
-  }),
-  babel: jsStandards()
+    devtool: 'source-map',
+    ignore: ['**/layout.html', '**/_*', '**/.*', 'readme.md', 'yarn.lock', 'package-lock.json'],
+    entry: {
+        'js/transliterate': ['./assets/js/transliterate.js'],
+        'js/remove': ['./assets/js/remove.js'],
+        'js/sequence': ['./assets/js/sequence.js'],
+        'js/index': ['./assets/js/index.js'],
+        'js/about': ['./assets/js/about.js']
+    },
+    reshape: htmlStandards({
+        locals: (ctx) => { return { pageId: pageId(ctx), foo: 'bar' } },
+        minify: env === 'production'
+    }),
+    postcss: cssStandards({
+        minify: env === 'production',
+        warnForDuplicates: env !== 'production'
+    }),
+    babel: jsStandards()
 }
