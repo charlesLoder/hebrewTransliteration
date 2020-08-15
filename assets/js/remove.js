6bfe748b0a35e48206f45272a70de71a6c021a13
Charles Loder <charlesloder@Charless-MacBook-Pro.local>
2020-08-14 23:38:51 -0400

    updated heb-trans 1.3.0

--- assets/js/remove.js
+++ assets/js/remove.js
@@ -1,12 +1,18 @@
-const inputBtn = document.querySelector('#input_button');
-const input = document.querySelector('#input');
-const output = document.querySelector('#output');
-const heb = require('hebrew-transliteration');
+const inputBtn = document.querySelector("#input_button");
+const input = document.querySelector("#input");
+const output = document.querySelector("#output");
+const heb = require("hebrew-transliteration");
 const remove = heb.remove;
 
-inputBtn.addEventListener('click', () => {
-  let removeVowelsVal = document.querySelector('#removeVowels').checked;
+inputBtn.addEventListener("click", () => {
+  let removeVowelsVal = document.querySelector("#removeVowels").checked;
+  let removeShinVal = document.querySelector("#removeShinDot").checked;
+  let removeSinVal = document.querySelector("#removeSinDot").checked;
   let hebText = input.value;
-  let transText = remove(hebText, {removeVowels: removeVowelsVal}); 
+  let transText = remove(hebText, {
+    removeVowels: removeVowelsVal,
+    removeShinDot: removeShinVal,
+    removeSinDot: removeSinVal,
+  });
   output.value = transText;
-})
+});
