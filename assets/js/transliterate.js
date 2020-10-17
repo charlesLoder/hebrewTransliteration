db5d7d68e6b1d681addfc833a13b9430cdae2002
Charles Loder <charlesloder@Charless-MacBook-Pro.local>
2020-10-17 13:19:04 -0400

    added check for niqqud

--- assets/js/transliterate.js
+++ assets/js/transliterate.js
@@ -1,34 +1,47 @@
-const inputBtn = document.querySelector('#input_button');
-const input = document.querySelector('#input');
-const output = document.querySelector('#output');
-const simpleBtn = document.querySelector('#isSimple');
-const qametsBtn = document.querySelector('#qametsQatan');
-const qametsInfo = document.querySelector('#qametsInfo');
-const heb = require('hebrew-transliteration');
+const inputBtn = document.querySelector("#input_button");
+const input = document.querySelector("#input");
+const output = document.querySelector("#output");
+const simpleBtn = document.querySelector("#isSimple");
+const qametsBtn = document.querySelector("#qametsQatan");
+const qametsInfo = document.querySelector("#qametsInfo");
+const heb = require("hebrew-transliteration");
 const transliterate = heb.transliterate;
 
-simpleBtn.addEventListener('change', (e) => {
-    if (e.target.checked) {
-        output.placeholder = 'ivrit';
-    } else {
-        output.placeholder = 'ʿibrît';
-    }
-})
+simpleBtn.addEventListener("change", (e) => {
+  if (e.target.checked) {
+    output.placeholder = "ivrit";
+  } else {
+    output.placeholder = "ʿibrît";
+  }
+});
 
 // start off hidden
 qametsInfo.hidden = true;
-qametsBtn.addEventListener('change', e => {
-    if (e.target.checked) {
-        qametsInfo.hidden = false;
-    } else {
-        qametsInfo.hidden = true;
-    }
-})
+qametsBtn.addEventListener("change", (e) => {
+  if (e.target.checked) {
+    qametsInfo.hidden = false;
+  } else {
+    qametsInfo.hidden = true;
+  }
+});
 
-inputBtn.addEventListener('click', () => {
+inputBtn.addEventListener("click", () => {
+  try {
     let qametsQatanVal = qametsBtn.checked;
-    let isSimpleVal = simpleBtn.checked
+    let isSimpleVal = simpleBtn.checked;
     let hebText = input.value;
-    let transText = transliterate(hebText, { isSequenced: true, qametsQatan: qametsQatanVal, isSimple: isSimpleVal });
+    const niqqud = /[\u{05B0}-\u{05BC},\u{05C7}]/u;
+    if (!niqqud.test(hebText)) {
+      alert(`The text entered does not have any niqqud (vowels).
+        This will only transliterate consonants.`);
+    }
+    let transText = transliterate(hebText, {
+      isSequenced: true,
+      qametsQatan: qametsQatanVal,
+      isSimple: isSimpleVal,
+    });
     output.value = transText;
-})
+  } catch (e) {
+    alert(`There was an error: ${e.message}`);
+  }
+});
