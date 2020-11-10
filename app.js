e3ead29caad608a5f0cff3fce33c12e0382a0edb
Charles Loder <charlesloder@MacBook-Pro.local>
2020-11-10 09:24:08 -0500

    removed /include from build

--- app.js
+++ app.js
@@ -1,26 +1,36 @@
-const htmlStandards = require('reshape-standard')
-const cssStandards = require('spike-css-standards')
-const jsStandards = require('spike-js-standards')
-const pageId = require('spike-page-id')
-const env = process.env.SPIKE_ENV
+const htmlStandards = require("reshape-standard");
+const cssStandards = require("spike-css-standards");
+const jsStandards = require("spike-js-standards");
+const pageId = require("spike-page-id");
+const env = process.env.SPIKE_ENV;
 
 module.exports = {
-    devtool: 'source-map',
-    ignore: ['**/layout.html', '**/_*', '**/.*', 'readme.md', 'yarn.lock', 'package-lock.json'],
-    entry: {
-        'js/transliterate': ['./assets/js/transliterate.js'],
-        'js/remove': ['./assets/js/remove.js'],
-        'js/sequence': ['./assets/js/sequence.js'],
-        'js/index': ['./assets/js/index.js'],
-        'js/about': ['./assets/js/about.js']
+  devtool: "source-map",
+  ignore: [
+    "**/layout.html",
+    "**/_*",
+    "**/.*",
+    "readme.md",
+    "yarn.lock",
+    "package-lock.json",
+    "./views/include/**",
+  ],
+  entry: {
+    "js/transliterate": ["./assets/js/transliterate.js"],
+    "js/remove": ["./assets/js/remove.js"],
+    "js/sequence": ["./assets/js/sequence.js"],
+    "js/index": ["./assets/js/index.js"],
+    "js/about": ["./assets/js/about.js"],
+  },
+  reshape: htmlStandards({
+    locals: (ctx) => {
+      return { pageId: pageId(ctx), foo: "bar" };
     },
-    reshape: htmlStandards({
-        locals: (ctx) => { return { pageId: pageId(ctx), foo: 'bar' } },
-        minify: env === 'production'
-    }),
-    postcss: cssStandards({
-        minify: env === 'production',
-        warnForDuplicates: env !== 'production'
-    }),
-    babel: jsStandards()
-}
+    minify: env === "production",
+  }),
+  postcss: cssStandards({
+    minify: env === "production",
+    warnForDuplicates: env !== "production",
+  }),
+  babel: jsStandards(),
+};
