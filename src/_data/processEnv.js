d935806b2259f28d523f9d5658b8a905739907fb
Charles Loder <cloder@classicalsubjects.com>
2022-03-16 17:15:06 -0400

    Add footer link to js package

--- src/_data/processEnv.js
+++ src/_data/processEnv.js
@@ -1,4 +1,8 @@
 require("dotenv").config();
+const ht = require("hebrew-transliteration/package.json");
 module.exports = {
   gaTagId: process.env.GA_TAG_ID,
+  htName: ht.name,
+  htHomepage: ht.homepage,
+  htVersion: ht.version,
 };
