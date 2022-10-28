909c18ee7b4dc0aa65248853daa39fb7397b28d9
Charles Loder <cloder@classicalsubjects.com>
2022-10-28 07:37:42 -0400

    Switch out esbuild pluginhnn

--- .eleventy.js
+++ .eleventy.js
@@ -1,19 +1,21 @@
 const htmlmin = require("html-minifier");
-const pluginESbuild = require("@jamshop/eleventy-plugin-esbuild");
+const esbuild = require("esbuild");
 const CleanCSS = require("clean-css");
 
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPlugin(pluginESbuild, {
-    entryPoints: {
-      transliterate: "src/assets/js/transliterate.js",
-      remove: "src/assets/js/remove.js",
-      friconix: "src/assets/js/friconix.js",
-      about: "src/assets/js/about.js",
-    },
-    output: "dist/assets",
-    esbuild: {
-      format: "esm",
-    },
+  eleventyConfig.on("afterBuild", () => {
+    return esbuild.build({
+      entryPoints: {
+        transliterate: "src/assets/js/transliterate.js",
+        remove: "src/assets/js/remove.js",
+        friconix: "src/assets/js/friconix.js",
+        about: "src/assets/js/about.js",
+      },
+      outdir: "dist/assets",
+      format: "iife",
+      bundle: true,
+      minify: true,
+    });
   });
 
   eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
@@ -34,6 +36,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/assets/img/hebrew-transliteration-card.png");
   // triggers a rebuild when anything in here changes
   eleventyConfig.addWatchTarget("src/assets/css/main.css");
+  eleventyConfig.addWatchTarget("src/assets/js/");
   eleventyConfig.addFilter("cssmin", function (code) {
     return new CleanCSS({}).minify(code).styles;
   });
