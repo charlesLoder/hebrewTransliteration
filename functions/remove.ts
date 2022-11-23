99f891ccdee58f0a5e4b5bf72eeae2b6ef4d8a97
Charles Loder <cloder@classicalsubjects.com>
2022-11-23 10:29:20 -0500

    Update function to match api

--- functions/remove.ts
+++ functions/remove.ts
@@ -1,18 +1,12 @@
 import { Handler, HandlerEvent } from "@netlify/functions";
 import { remove } from "hebrew-transliteration";
 
-type RemoveOptions = {
-  removeVowels: boolean;
-  removeShinDot: boolean;
-  removeSinDot: boolean;
-};
-
 const handler: Handler = async (event: HandlerEvent, context) => {
   try {
     if (event.httpMethod !== "POST") throw new Error(`${event.httpMethod} Not Allowed`);
     if (!event.body) throw new Error("No event body");
 
-    const body: { text: string; options: RemoveOptions } = JSON.parse(event.body);
+    const body: { text: string; options } = JSON.parse(event.body);
     const removed = remove(body.text, body.options);
     const response = { text: removed };
     return {
