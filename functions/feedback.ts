42ea99218a7c5def1a04c7e3bd993929b64c7205
Charles Loder <cloder@classicalsubjects.com>
2023-04-07 19:59:30 -0400

    Add feedback function

---
+++ functions/feedback.ts
@@ -0,0 +1,52 @@
+import { Handler, HandlerEvent } from "@netlify/functions";
+import { GoogleSpreadsheet } from "google-spreadsheet";
+
+/**
+ * send errors from transliterate page to Google sheet
+ * @param event
+ * @param context
+ * @returns figure out what is returned from sheets
+ */
+const handler: Handler = async (event: HandlerEvent, context) => {
+  try {
+    if (event.httpMethod !== "POST") throw new Error(`${event.httpMethod} Not Allowed`);
+    if (!event.body) throw new Error("No event body");
+
+    const doc = new GoogleSpreadsheet(process.env.SHEET_ID || "");
+    await doc.useServiceAccountAuth({
+      client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL || "",
+      private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/gm, "\n") || "",
+    });
+    await doc.loadInfo();
+    const sheet = doc.sheetsByTitle[process.env.SHEET_FEEDBACK_TITLE || ""];
+
+    const body: { issue: string; response: string; email?: string } = JSON.parse(event.body);
+    const resp = await sheet.addRow({
+      Date: new Date().toLocaleDateString("en-CA").toString(),
+      Issue: body.issue,
+      Feedback: body.response,
+      Email: body.email || "",
+    });
+
+    return {
+      statusCode: 200,
+      headers: {
+        "access-control-allow-origin": "*",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ message: `Wrote to row ${resp?._rowNumber || 0}` }),
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      statusCode: 400,
+      headers: {
+        "access-control-allow-origin": "*",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(error),
+    };
+  }
+};
+
+export { handler };
