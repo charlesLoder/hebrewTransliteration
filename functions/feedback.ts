9b241ea363df6cb84573c6eeb7a9f4b808743ab6
Charles Loder <charles.w.loder@gmail.com>
2024-01-23 22:44:30 -0500

    Sanitize key input
    - https://stackoverflow.com/a/73686717/10236980

--- functions/feedback.ts
+++ functions/feedback.ts
@@ -15,7 +15,7 @@ const handler: Handler = async (event: HandlerEvent, context) => {
 
     const serviceAccountAuth = new JWT({
       email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
-      key: process.env.GOOGLE_PRIVATE_KEY,
+      key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
       scopes: ["https://www.googleapis.com/auth/spreadsheets"],
     });
 
