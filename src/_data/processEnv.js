d3d10438ceda876802681e79219a1ca74e5efaa5
charlesLoder <charles.w.loder@gmail.com>
2026-03-26 21:44:24 -0400

    Prune everything for rewrite

--- src/_data/processEnv.js
+++
@@ -1,8 +0,0 @@
-require("dotenv").config();
-const ht = require("hebrew-transliteration/package.json");
-module.exports = {
-  gaTagId: process.env.GA_TAG_ID,
-  htName: ht.name,
-  htHomepage: ht.homepage,
-  htVersion: ht.version,
-};
